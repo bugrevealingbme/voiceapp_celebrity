62acde9785babf83883645823fdcbc382e0e58cb
Burak Mete Erdoğan <bugrevealingbme@gmail.com>
2023-08-25 22:38:50 +0300

    ios onesignal added

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Burak Mete Erdoğan on 25.08.2023.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end

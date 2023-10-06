881b458079d9ced3d39457a35748eaa28179a0dc
Burak Mete Erdoğan <bugrevealingbme@gmail.com>
2023-10-06 21:09:10 +0300

    free rights reduced

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
 final ValueNotifier<bool> upgraded = ValueNotifier<bool>(false);
-final ValueNotifier<int> rights = ValueNotifier<int>(2);
+final ValueNotifier<int> rights = ValueNotifier<int>(1);
 bool showedIad = false;

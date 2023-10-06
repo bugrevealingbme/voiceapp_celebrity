68d191cda7850e08bbc71acb0b118bee8880505b
Burak Mete Erdoğan <bugrevealingbme@gmail.com>
2023-10-06 21:53:14 +0300

    fx ios

--- lib/main.dart
+++ lib/main.dart
@@ -41,9 +41,9 @@ main() async {
   upgraded.value = prefs.getBool("upgraded") ?? false;
 
   //
-  OneSignal.shared.setLogLevel(OSLogLevel.verbose, OSLogLevel.none);
-  OneSignal.shared.setAppId("535e15d0-73f7-41cf-94e8-74d57ebc4e23");
-  OneSignal.shared.promptUserForPushNotificationPermission();
+  OneSignal.Debug.setLogLevel(OSLogLevel.verbose);
+  OneSignal.initialize("535e15d0-73f7-41cf-94e8-74d57ebc4e23");
+  OneSignal.Notifications.requestPermission(true);
   //
 
   await Firebase.initializeApp(
